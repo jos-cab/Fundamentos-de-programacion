77f52557cc1b2faff57583621a2734589e066a85
jos-cabrera <joseacabrera@protonmail.com>
2024-11-04 11:34:37 -0300

    Cambio tipo de dato de suma_naturales y uso una fórmula en vez de un bucle

--- Funciones/3.c
+++ Funciones/3.c
@@ -4,22 +4,23 @@
 
 #include <stdio.h>
 
-unsigned long long suma_naturales(int n)
+unsigned long int suma_naturales(int n)
 {
-    unsigned long long suma = 0;
-    int i;
+    unsigned long int suma;
 
-    // Alternativamente se puede usar la fórmula: [ n.(n+1) ] / 2
-    for (i = 0; i <= n; i++)
-        suma += i;
+    if(n > 0)
+        suma = (n * (n + 1)) / 2;
+    else
+        suma = 0;
 
     return suma;
 }
 
 int main()
 {
-    printf("La suma natural de -1: %d\n", suma_naturales(-1));   
-    printf("La suma natural de 0: %d\n", suma_naturales(0));   
+    printf("La suma natural de -2: %d\n", suma_naturales(-2));
+    printf("La suma natural de -1: %d\n", suma_naturales(-1));
+    printf("La suma natural de 0: %d\n", suma_naturales(0)); 
     printf("La suma natural de 1: %d\n", suma_naturales(1));
     printf("La suma natural de 2: %d\n", suma_naturales(2));
     printf("La suma natural de 10: %d\n", suma_naturales(10));
