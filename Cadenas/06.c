01544290e9d2e57d2ab9998e64e384157a610e7e
jos-cabrera <joseacabrera@protonmail.com>
2024-11-05 17:27:05 -0300

    Agrego ejercicio 6 de cadenas

---
+++ Cadenas/06.c
@@ -0,0 +1,21 @@
+/*
+6. Escribir una función para validar una nueva clave de acceso
+
+La función deberá recibir una cadena de caracteres, que contendrá la clave candidata,
+ingresada previamente por el usuario.
+
+Devolverá true o false, dependiendo de si cumple o no con las siguientes condiciones:
+- La clave debe estar formada únicamente por, entre 4 y 8 caracteres numéricos
+- Los caracteres no pueden ser todos iguales
+
+Ejemplos:
+validar("j2020") devuevle false
+validar("2021a") devuevle false
+validar("20X21") devuevle false
+validar("2220") devuelve true
+validar("23445776") devuelve true
+validar("089") devuelve false
+validar("027845321") devuelve false
+validar("02784532") devuelve true
+validar("33333") devuelve false
+*/
