4ffd201e41f549af99498e40a092d3813acfff6d
jos-cabrera <joseacabrera@protonmail.com>
2024-11-05 18:49:49 -0300

    Agrego ejercicio 1 de registros

---
+++ Registros/01.c
@@ -0,0 +1,13 @@
+/*
+1) Se tienen los siguientes datos de los empleados de una empresa:
+Apellido y Nombre (35 caracteres)
+Fecha de Nacimiento (día, mes y año)
+Fecha de Ingreso (día, mes y año)
+
+Sueldo Básico
+Se conoce la cantidad N de empleados y la fecha de procesamiento.
+
+Se pide: Imprimir un listado con los apellidos y nombres de las personas
+que cumplan años en el mes y calcular e informar una suma de regalo
+equivalente al uno por mil del sueldo por cada año cumplido.
+*/
