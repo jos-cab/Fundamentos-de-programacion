90bd29b9f5365dc3b540a18ee95774dc7a5bb276
jos-cabrera <joseacabrera@protonmail.com>
2024-11-07 16:34:45 -0300

    Agrego resolución ejercicio 6 de cadenas

--- Cadenas/06.c
+++ Cadenas/06.c
@@ -21,6 +21,90 @@ validar("02784532") devuelve true
 validar("33333") devuelve false
 */
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX 32
+
+typedef char t_cadena[MAX];
+
+bool son_todos_caracteres_iguales(t_cadena clave, int ML)
+{
+    int i;
+    bool son_todos_iguales;
+    char caracter_bandera;
+
+    son_todos_iguales = true;
+    caracter_bandera = clave[0];
+    
+    i = 1; // clave[0] ya está guardado en caracter_bandera
+    while (son_todos_iguales && i < ML)
+    {
+        son_todos_iguales = clave[i] == caracter_bandera;
+        i++;
+    }
+    
+    return son_todos_iguales;
+}
+
+bool son_todos_caracteres_numericos(t_cadena clave, int ML)
+{
+    bool son_todos_numericos;
+    int i;
+
+    son_todos_numericos = true;
+    i = 0;
+
+    while (son_todos_numericos && i < ML)
+    {
+        switch (clave[i])
+        {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                son_todos_numericos = true;
+                break;
+
+            default:
+                son_todos_numericos = false;
+                break;
+        }
+
+        i++;
+    }
+
+    return son_todos_numericos;
+}
+
+bool validar(t_cadena clave)
+{
+    bool clave_es_valida;
+    unsigned short int cantidad_caracteres_clave;
+    int i;
+    
+    cantidad_caracteres_clave = strlen(clave);
+
+    clave_es_valida = true;
+    
+    if (cantidad_caracteres_clave < 4 || cantidad_caracteres_clave > 8)
+        clave_es_valida = false;
+    else if(son_todos_caracteres_iguales(clave, cantidad_caracteres_clave))
+        clave_es_valida = false;
+    else if (!(son_todos_caracteres_numericos(clave, 
+                                              cantidad_caracteres_clave)))
+        clave_es_valida = false;
+    
+    return clave_es_valida;
+}
+
 int main()
 {
     if (validar("j2020")) printf("La clave 'j2020' es válida\n"); 
