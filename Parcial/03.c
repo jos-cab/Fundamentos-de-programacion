3f7ea1e0a2d54dc331522ebe098d451c35aab5b1
jos-cabrera <joseacabrera@protonmail.com>
2024-11-07 10:08:33 -0300

    Resuelvo el tercer ejercicio de parcial

--- Parcial/03.c
+++ Parcial/03.c
@@ -5,3 +5,35 @@ TMatriz ya cargada, sus respectivos maximos logicos y un numero entero.
 Devolver True si el numero entero se encuentra en la matriz o false si no lo
 está.
 */
+
+#include <stdbool.h>
+
+// No recuerdo los máximos lógicos que pedía originalmente
+#define MAX 45
+
+typedef int TMatriz[MAX][MAX];
+
+void buscar_matriz(TMatriz matriz, int MLF, int MLC, 
+                   int x, 
+                   bool *numero_en_matriz)
+{
+    int i, j;
+
+    // Se debe definir la variable para compararla en los while
+    *numero_en_matriz = false;
+
+    i = 0;
+
+    while (!*numero_en_matriz && i < MLF)
+    {
+        j = 0;
+
+        while (!*numero_en_matriz && j < MLC)
+        {
+            *numero_en_matriz = matriz[i][j] == x;
+            j++;
+        }
+
+        i++;
+    }
+}
