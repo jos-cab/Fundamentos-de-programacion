0e5de6315c6afcff88a7e7d05522c09f107b18c2
jos-cabrera <joseacabrera@protonmail.com>
2024-11-07 15:52:48 -0300

    Establezco los casos del enunciado

--- Cadenas/06.c
+++ Cadenas/06.c
@@ -20,3 +20,35 @@ validar("027845321") devuelve false
 validar("02784532") devuelve true
 validar("33333") devuelve false
 */
+
+int main()
+{
+    if (validar("j2020")) printf("La clave 'j2020' es válida\n"); 
+    else printf("La clave 'j2020' no es válida\n");
+    
+    if (validar("2021a")) printf("La clave '2021a' es válida\n"); 
+    else printf("La clave '2021a' no es válida\n");
+
+    if (validar("20X21")) printf("La clave '20X21' es válida\n");
+    else printf("La clave '20X21' no es válida\n");
+
+    if (validar("2220")) printf("La clave '2220' es válida\n");
+    else printf("La clave '2220' no es válida\n");
+
+    if (validar("23445776")) printf("La clave '23445776' es válida\n");
+    else printf("La clave '23445776' no es válida\n");
+
+    if (validar("089")) printf("La clave '089' es válida\n");
+    else printf("La clave '089' no es válida\n");
+
+    if (validar("027845321")) printf("La clave '027845321' es válida\n");
+    else printf("La clave '027845321' no es válida\n");
+
+    if (validar("02784532")) printf("La clave '02784532' es válida\n");
+    else printf("La clave '02784532' no es válida\n");
+
+    if (validar("33333")) printf("La clave '33333' es válida\n");
+    else printf("La clave '33333' no es válida\n");
+
+    return 0;
+}
