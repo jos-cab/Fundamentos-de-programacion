16ffabf248722bad4c380101d07ae0ad4a6a88b2
jos-cabrera <joseacabrera@protonmail.com>
2024-11-07 18:28:43 -0300

    Agrego enunciado ejercicio 9

---
+++ Cadenas/09.c
@@ -0,0 +1,16 @@
+/*
+9. Escribir una función que recibirá por parámetro, una palabra, que representa
+un sustantivo en singular.
+
+La función deberá devolver, el plural de dicho sustantivo, aplicando las
+siguientes reglas:
+
+a. Agregar una “s” al final, si la palabra termina en vocal sin acento.
+b. Agregar una “s” al final, si la palabra termina con una é (acentuada).
+c. Si la palabra termina en “z”, la reemplazamos por “ces”.
+d. Agregamos “es” al final, si la palabra termina en una consonante (a excepción
+de la “s”, la “z”, y la “x”), ó si la palabra termina con las vocales
+acentuadas: á, í, ó, ú.
+e. Si el sustantivo termina en “s” ó “x”, entonces el plural es igual al
+singular, por lo tanto la función deberá devolver lo mismo que recibió.
+*/
