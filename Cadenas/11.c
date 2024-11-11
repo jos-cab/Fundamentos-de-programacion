739c6eba5f1de2c25157926e7699563407049513
jos-cabrera <joseacabrera@protonmail.com>
2024-11-11 12:49:12 -0300

    Agrego ejercicio 11 de cadenas

---
+++ Cadenas/11.c
@@ -0,0 +1,10 @@
+/*
+11. Escribir un programa modular en C, que solicite el ingreso de no más de 20 
+palabras.
+
+Cada palabra no tendrá más de 20 caracteres. Informar:
+1. Mostrar las palabras ingresadas.
+2. Cuál es la palabra más larga.
+3. Solictar el ingreso de una palabra, e indicar si la misma se encuentra entre
+las ingresadas.
+*/
