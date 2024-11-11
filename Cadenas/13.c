ea4cfce4e00621425ccfcf7d4f7ba11ae98de85d
jos-cabrera <joseacabrera@protonmail.com>
2024-11-11 12:49:24 -0300

    Agrego ejercicio 13 de cadenas

---
+++ Cadenas/13.c
@@ -0,0 +1,10 @@
+/*
+13. Escribir un programa modular en C, que solicite el ingreso de 3 oraciones,
+de no más de 50 caracteres cada una.
+
+Luego informar:
+1. Cuál es la oración más larga.
+2. Si hay al menos 2 oraciones iguales.
+3. Solicitar el ingreso de una palabra o parte de una oración, e indicar si la
+misma se encuentra en las oraciones, y en cuales.
+*/
