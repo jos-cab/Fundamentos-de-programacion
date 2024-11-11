ff68b20a35cd663751d718453af25af59074d1fc
jos-cabrera <joseacabrera@protonmail.com>
2024-11-11 12:49:18 -0300

    Agrego ejercicio 12 de cadenas

---
+++ Cadenas/12.c
@@ -0,0 +1,7 @@
+/*
+12. Escribir un programa modular en C, que solicite el ingreso de 1 oración, de 
+no más de 100 caracteres.
+
+Luego informar la cantidad de vocales minúsculas, no acentuadas, que hay en la
+oración.
+*/
