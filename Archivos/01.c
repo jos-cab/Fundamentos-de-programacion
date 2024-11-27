5023c03f2cac1fffe6e9be02c0b262104ba585b9
jos-cabrera <joseacabrera@protonmail.com>
2024-11-27 12:31:07 -0300

    Agrego resolución ejercicio 1 de archivos

---
+++ Archivos/01.c
@@ -0,0 +1,39 @@
+/*
+1) Dado un archivo de números enteros mayores o iguales que 0, generar otro
+archivo que almacene los factoriales de cada número del registro correspondiente
+del archivo de entrada.
+*/
+
+#include <stdio.h>
+
+long long obtener_factorial(int numero)
+{
+    long long factorial = 1;
+
+    for (int i = 2; i <= numero; i++)
+        factorial *= i;
+
+    return factorial;
+}
+
+int main()
+{
+    FILE *archivo_numeros, *archivo_factoriales;
+    int numero;
+    long long factorial;
+
+    archivo_numeros = fopen("./numeros.txt", "r");
+    archivo_factoriales = fopen("./factoriales.txt", "w");
+
+    while (!feof(archivo_numeros))
+    {
+        fscanf(archivo_numeros, "%d", &numero);
+        factorial = obtener_factorial(numero);
+        fprintf(archivo_factoriales, "%lld\n", factorial);
+    }
+
+    fclose(archivo_numeros);
+    fclose(archivo_factoriales);
+
+    return 0;
+}
