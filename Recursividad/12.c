b4ad4acce9c6d71e2a96be340f1afb1cd01f9b92
jos-cabrera <joseacabrera@protonmail.com>
2024-12-02 13:13:23 -0300

    Agrego resolución ejercicio 12 de recursividad y modifico gitignore para que ignore tempCodeRunnerFile.c

--- Recursividad/12.c
+++ Recursividad/12.c
@@ -2,3 +2,26 @@
 12) Desarrollar un programa que muestre los primeros N números naturales. El
 numero N lo debe ingresar el usuario.
 */
+
+#include <stdio.h>
+
+void mostrar_numeros(int n)
+{
+	if (n > 0)
+	{
+		mostrar_numeros(n - 1);
+		printf("%d ", n);
+	}
+}
+
+int main()
+{
+	int n;
+
+	printf("Ingrese un numero: ");
+	scanf("%d", &n);
+
+	mostrar_numeros(n);
+
+	return 0;
+}
