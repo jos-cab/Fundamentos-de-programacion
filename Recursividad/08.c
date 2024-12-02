c16301c1dbebe84aca24ac4b8b6fa58ff0522895
jos-cabrera <joseacabrera@protonmail.com>
2024-12-02 12:40:59 -0300

    Agrego resolución ejercicio 8 de recursividad

--- Recursividad/08.c
+++ Recursividad/08.c
@@ -6,15 +6,72 @@ parámetro es capicúa.
 #include <stdio.h>
 #include <stdbool.h>
 
+int calcular_numero_invertido(int numero)
+{
+	int invertido, digito;
+
+	invertido = 0;
+
+	while (numero > 0)
+	{
+		digito = numero % 10;
+		numero /= 10;
+
+		invertido = invertido * 10 + digito;
+	}
+
+	return invertido;
+}
+
+int elevado(int base, int exponente)
+{
+	int resultado;
+
+	resultado = 1;
+
+	while (exponente > 0)
+	{
+		resultado *= base;
+		exponente--;
+	}
+
+	return resultado;
+}
+
 bool es_capicua(int numero)
 {
+	int primer_digito, ultimo_digito;
+
+	primer_digito = calcular_numero_invertido(numero) % 10;
+	ultimo_digito = numero % 10;
+
+	if ((primer_digito == 0) && (ultimo_digito == 0))
+		return true;
+	else if (primer_digito == ultimo_digito)
+	{
+		// saco el primer dígito
+		numero = calcular_numero_invertido(numero) / 10;
+		numero = calcular_numero_invertido(numero);
+
+		// saco el ultimo dígito
+		numero /= 10;
+
+		return es_capicua(numero);
+	}
+
+	return false;
 }
 
 int main()
 {
 	int numero;
 
-	numero = 123321;
+	numero = 151;
+
+	if (es_capicua(numero))
+		printf("%d Es capicua", numero);
+	else
+		printf("%d No es capicua", numero);
 
 	return 0;
 }
