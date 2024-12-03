4e25bd28d9d347f01af059be48c211b30c983fe0
jos-cabrera <joseacabrera@protonmail.com>
2024-12-02 21:30:39 -0300

    Agrego resolución ejercicio 8 de archivos

--- Archivos/08/08.c
+++ Archivos/08/08.c
@@ -12,50 +12,45 @@ int main()
 {
 	FILE *archivo_texto;
 	char caracter;
-	int cantidad_monosilabos = 0;
-	int cantidad_palabras = 0;
-	int longitud_palabra = 0;
-	int longitud_total = 0;
+
+	int cantidad_monosilabos, cantidad_palabras, longitud_palabra, longitud_total, longitud_actual;
+
+	cantidad_monosilabos = 0;
+	cantidad_palabras = 0;
+	longitud_palabra = 0;
+	longitud_total = 0;
 
 	archivo_texto = fopen("./texto_sin_espacios.txt", "r");
 
-	/*	while (!feof(archivo_texto))
+	while (!feof(archivo_texto))
+	{
+		caracter = fgetc(archivo_texto);
+
+		if (0 < caracter && caracter <= 255)
 		{
-			caracter = fgetc(archivo_texto);
-
-			if (0 < caracter && caracter <= 255)
+			if (('a' <= caracter && caracter <= 'z') || ('A' <= caracter && caracter <= 'Z'))
 			{
-				switch (caracter)
-				{
-				case 'a':
-				case 'e':
-				case 'i':
-				case 'o':
-				case 'u':
-					cantidad_monosilabos++;
-					break;
-
-				case ' ':
-				case '\n':
-					if (longitud_palabra > 0)
-					{
-						longitud_total += longitud_palabra;
-						cantidad_palabras++;
-					}
-
-					longitud_palabra = 0;
-					break;
-				}
+				longitud_palabra++;
 			}
+			else if (longitud_palabra > 0)
+			{
+				longitud_total += longitud_palabra;
 
-			longitud_palabra++;
+				// TODO: Hacer una función para hacer la comprobación de monosílabos
+				if (longitud_palabra < 3)
+					cantidad_monosilabos++;
+
+				longitud_palabra = 0;
+				cantidad_palabras++;
+			}
 		}
-	*/
+	}
+
 	fclose(archivo_texto);
 
 	printf("Cantidad de monosilabos: %d\n", cantidad_monosilabos);
 	printf("Cantidad de palabras: %d\n", cantidad_palabras);
 	if (cantidad_palabras > 0)
-		printf("Longitud promedio de las palabras: %d\n", longitud_total / cantidad_palabras);
+		printf("Longitud promedio de las palabras: %.2g\n", (float)longitud_total / cantidad_palabras);
 	return 0;
 }
