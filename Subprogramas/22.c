e16976a7a7917fb4fac51e414c597bcc09624590
jos-cabrera <joseacabrera@protonmail.com>
2024-12-04 15:08:09 -0300

    Agrego resoluciones ejercicios 22 y 23 de subprogramas con biblioteca math.h

--- Subprogramas/22.c
+++ Subprogramas/22.c
@@ -1,7 +1,48 @@
 /*
-22)Escribir un subprograma que dadas 2 coordenadas rectangulares (x,y)
-devuelva las correspondientes coordenadas polares r = ( x2 + y2 )½
-y α =
-arctg ( y / x ) .
+22) Escribir un subprograma que dadas 2 coordenadas rectangulares (x,y) devuelva
+las correspondientes coordenadas polares
 
+r = ( x2 + y2 )½
+y α = arctg ( y / x ).
 */
+
+#include <stdio.h>
+#include <math.h>
+
+// Hago las funciones por si luego quiero hacerlas sin math.h
+
+double elevado(double x, double y)
+{
+	return pow(x, y);
+}
+
+double arctg(double x)
+{
+	return atan(x);
+}
+
+void imprimir_coordenadas_polares(double x, double y)
+{
+	double r, a;
+
+	// Elevo a la 0.5 para sacar raíz cuadrada
+	r = elevado((elevado(x, 2) + elevado(y, 2)), 0.5);
+	a = arctg(y / x);
+
+	printf("Coordenadas polares: r = %.2lf, a = %.2lf\n", r, a);
+}
+
+int main()
+{
+	double x, y;
+
+	printf("Ingrese x: ");
+	scanf("%lf", &x);
+
+	printf("Ingrese y: ");
+	scanf("%lf", &y);
+
+	imprimir_coordenadas_polares(x, y);
+
+	return 0;
+}
--- Subprogramas/23.c
+++ Subprogramas/23.c
@@ -1,4 +1,45 @@
 /*
-23)Escribir un subprograma que dadas las coordenadas polares r y α devuelva
+23) Escribir un subprograma que dadas las coordenadas polares r y α devuelva
 las correspondientes coordenadas rectangulares x = r cos α , y = r sen α.
 */
+
+#include <stdio.h>
+#include <math.h>
+
+// Hago las funciones por si luego quiero hacerlas sin math.h
+
+// NO FUNCIONA
+/*double cos(double x)
+{
+	return cos(x);
+}*/
+
+double sen(double x)
+{
+	return sin(x);
+}
+
+void imprimir_coordenadas_rectangulares(double r, double a)
+{
+	double x, y;
+
+	x = r * cos(a);
+	y = r * sen(a);
+
+	printf("Coordenadas rectangulares: x = %.2lf, y = %.2lf\n", x, y);
+}
+
+int main()
+{
+	double r, a;
+
+	printf("Ingrese r: ");
+	scanf("%lf", &r);
+
+	printf("Ingrese a: ");
+	scanf("%lf", &a);
+
+	imprimir_coordenadas_rectangulares(r, a);
+
+	return 0;
+}
