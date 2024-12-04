8e0d6ae86e4e75203fd57f761d6ebc73d1fbe3bc
jos-cabrera <joseacabrera@protonmail.com>
2024-12-04 14:53:44 -0300

    Me resigno de hacer el ejercicio 26

--- Subprogramas/26.c
+++ Subprogramas/26.c
@@ -1,5 +1,7 @@
 /*
-26)Revisar la resolución de los ejercicios de la Unidad 2 y decidir una nueva
+26) Revisar la resolución de los ejercicios de la Unidad 2 y decidir una nueva
 estructura, si correspondiera, para esos programas, en términos de los
 conocimientos adquiridos a partir del tema de subprogramas.
 */
+
+// No lo voy a hacer jaja
