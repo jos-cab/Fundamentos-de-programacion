43ad60acae07a24e89308faecfb8b75143a87bd1
jos-cabrera <joseacabrera@protonmail.com>
2024-12-04 21:15:35 -0300

    Agrego 0 a los nombres de los ejercicios de memoria dinámica

--- Guías/10. Memoria dinámica/1.c
+++
@@ -1,6 +0,0 @@
-/*
-1) Escribir un programa el cual reserve memoria dinámica para almacenar un
-número entero (int), le solicite al usuario el ingreso de un número y se asigna
-dicho valor en la memoria reservada, luego mostrar dicho valor por pantalla
-Liberar la memoria reservada al finalizar el programa.
-*/
--- Guías/10. Memoria dinámica/2.c
+++
@@ -1,7 +0,0 @@
-/*
-2) Escribir un programa el cual reserve memoria dinámica para almacenar una
-cierta cantidad de números enteros (n * int), este valor n debe ser ingresado por
-el usuario. Luego solicitarle que ingresé n valores enteros ingresados de a uno y
-almacenarlos en la memoria previamente reservada. Mostrar luego todos los
-valores ingresados. Liberar la memoria reservada al finalizar el programa.
-*/
