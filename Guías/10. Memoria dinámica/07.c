8a14a1c3ded6a6261a6ea652d0d001427a77b024
jos-cabrera <joseacabrera@protonmail.com>
2024-12-05 15:41:23 -0300

    Agrego resolución ejercicio 7 de memoria dinámica

--- Guías/10. Memoria dinámica/07.c
+++ Guías/10. Memoria dinámica/07.c
@@ -1,14 +1,53 @@
 /*
 7) Implementar una función que retorna un puntero a un vector de n struct del
-tipo t_alumno (ver anexo), el cual apunta a un bloque de memoria dinámica,
-respetando la siguiente declaración. En caso de no poder reservar la memoria,
-retornar NULL.
+tipo t_alumno, el cual apunta a un bloque de memoria dinámica, respetando la
+siguiente declaración.
+
+En caso de no poder reservar la memoria, retornar NULL.
 
 t_alumno* crear_alumnos(int n);
 
 typedef struct {
-int padron;
-char nombre[30];
-char apellido[30];
+	int padron;
+	char nombre[30];
+	char apellido[30];
 } t_alumno;
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	int padron;
+	char nombre[30];
+	char apellido[30];
+} t_alumno;
+
+t_alumno *crear_alumnos(int n)
+{
+	// return (t_alumno *)malloc(n * sizeof(t_alumno));
+	return malloc(n * sizeof(t_alumno));
+}
+
+int main()
+{
+	int n;
+	t_alumno *alumnos;
+
+	n = 3;
+	alumnos = crear_alumnos(n);
+
+	if (alumnos != NULL)
+	{
+		printf("Alumnos creados conxito\n");
+
+		free(alumnos);
+	}
+	else
+	{
+		printf("No se pudo reservar la memoria");
+	}
+
+	return 0;
+}
