3d5636784fba5b0c09e7f6b1640f032f0a37c13a
jos-cabrera <joseacabrera@protonmail.com>
2024-12-07 09:52:04 -0300

    Empiezo la resolución del ejercicio 1 de TDA

--- Guías/11. TDA/01.c
+++ Guías/11. TDA/01.c
@@ -35,22 +35,33 @@ typedef struct
 
 void crear(double x, double y, double lado, t_cuadrado c)
 {
+	if (lado > 0)
+	{
+		c.x_coord = x;
+		c.y_coord = y;
+		c.lado = lado;
+	}
 }
 
 double perimetro(t_cuadrado c)
 {
+	return 4 * c.lado;
 }
 
 double area(t_cuadrado c)
 {
+	return c.lado * c.lado;
 }
 
 void redimensionar(t_cuadrado c, double factor)
 {
+	c.lado *= factor;
 }
 
 bool punto_interno(t_cuadrado c, double x, double y)
 {
+	return (x >= c.x_coord && x <= c.x_coord + c.lado) &&
+		   (y >= c.y_coord && y <= c.y_coord + c.lado);
 }
 
 int main()
